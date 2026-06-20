7724cb1d004fd70a9c651ee8c18772658be2e555
sanjaikumarsiddarth-blip <sanjaikumarsiddarth@gmail.com>
2026-06-20 13:14:35 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -20,3 +20,49 @@ counters.forEach(counter => {
 
     update();
 });
+
+function sendWhatsApp(){
+
+    const name =
+    document.getElementById("name").value;
+
+    const phone =
+    document.getElementById("phone").value;
+
+    const location =
+    document.getElementById("location").value;
+
+    const rice =
+    document.getElementById("rice").value;
+
+    const quantity =
+    document.getElementById("quantity").value;
+
+    const message =
+    document.getElementById("message").value;
+
+    const text =
+
+`🌾 Traditional Rice Vijayashri Order
+
+Name : ${name}
+
+Phone : ${phone}
+
+Location : ${location}
+
+Rice Variety : ${rice}
+
+Quantity : ${quantity}
+
+Additional Notes :
+${message}`;
+
+    const whatsappNumber =
+    "918825668558";
+
+    const url =
+    `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(text)}`;
+
+    window.open(url, "_blank");
+}
